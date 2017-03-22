04ff7c8e6d105dfe20035d115c321479104a65f3
happydxh <1109747853@qq.com>
2017-03-22 10:43:15 +0800

    update

--- web/bjphp/config/fileconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-	'attpath'	=>	INDEX_PATH . '/attachment',//附件路径（真实文件的绝对路径），不以 '/' 结尾
-	'atturl'	=>	'/attachment',//附件网址的根路径
-];
--- web/policy/user/User.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace policy\user;
-class User{
-
-}
